e35e8ef4c1e15f765d984fec122985de8fd05d29
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-28 22:12:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:user]["username"])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error
 
   end
 
